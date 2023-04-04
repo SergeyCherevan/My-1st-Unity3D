283c2d7c842700bae1b7faa902c2752b3c91c8b6
SergeyCherevan <CherevanSergeyLugansk@gmail.com>
2023-04-04 21:12:58 +0300

    add: connection to server, PhotonManager class

---
+++ Assets/Scripts/PhotonManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PhotonManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    string region;
+    
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        PhotonNetwork.ConnectToRegion(region);
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log($"Вы подключились: {PhotonNetwork.CloudRegion}");
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.Log($"Вы отключились от сервера!");
+    }
+}
