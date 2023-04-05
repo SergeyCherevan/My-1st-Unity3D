684d78e29e3ecd6139fb4e42adf7309ee6f17a57
SergeyCherevan <CherevanSergeyLugansk@gmail.com>
2023-04-05 06:23:17 +0300

    add: CreateRoomButton() method to PhotonManager class

--- Assets/Scripts/PhotonManager.cs
+++ Assets/Scripts/PhotonManager.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
+
 using Photon.Pun;
 using Photon.Realtime;
 
+using TMPro;
+
 public class PhotonManager : MonoBehaviourPunCallbacks
 {
-    [SerializeField]
-    string region;
-    
+    public string region;
+    public TMP_InputField RoomNameInput;
+
     void Start()
     {
         PhotonNetwork.ConnectUsingSettings();
@@ -15,11 +18,34 @@ public class PhotonManager : MonoBehaviourPunCallbacks
 
     public override void OnConnectedToMaster()
     {
-        Debug.Log($"Вы подключились: {PhotonNetwork.CloudRegion}");
+        if (!PhotonNetwork.IsConnected)
+        {
+            return;
+        }
+
+        Debug.Log($"Вы подключились. Регион: {PhotonNetwork.CloudRegion}");
     }
 
     public override void OnDisconnected(DisconnectCause cause)
     {
         Debug.Log($"Вы отключились от сервера!");
+    }
+
+    public void CreateRoomButton()
+    {
+        RoomOptions options = new RoomOptions();
+        options.MaxPlayers = 2;
+
+        PhotonNetwork.CreateRoom(RoomNameInput.text, options, TypedLobby.Default);
+    }
+
+    public override void OnCreatedRoom()
+    {
+        Debug.Log($"Создана комната. Её имя: {PhotonNetwork.CurrentRoom.Name}");
+    }
+
+    public override void OnCreateRoomFailed(short returnedCode, string message)
+    {
+        Debug.Log($"Не удалось создать комнату. Код ошибки: {returnedCode}. Сообщение: {message}");
     }
 }
